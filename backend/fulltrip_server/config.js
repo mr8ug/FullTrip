641eb8d801feddc409aebf93208fb353f468688e
W Guay Sen Rafael Herrador Reyes <2214940300101@ingenieria.usac.edu.gt>
2022-10-02 15:39:54 -0600

    se reconfiguro  de config

--- backend/fulltrip_server/config.js
+++ backend/fulltrip_server/config.js
@@ -1,9 +1,11 @@
 const config ={
     db:{
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        credentials:{
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        }
     },
     listPerPage: 10,
 
