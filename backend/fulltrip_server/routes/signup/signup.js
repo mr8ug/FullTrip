e1fe58a1aa00b6d6212797c833c7315c65c8f779
josejfss <josejfss98@gmail.com>
2022-10-03 08:51:12 -0600

    signup enpoint

---
+++ backend/fulltrip_server/routes/signup/signup.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+module.exports = (express,app) => {
+    
+	app.post('/api/signup',upload.any(), async function(req,res){
+        const conn = await mysql.createConnection(app.config.db.credentials);
+        const {full_name, date_birth = '', email, nickname = '', password, type_user, country = '', city = ''} = req.body
+        
+        console.log('Hola: ', full_name)
+        if (full_name == undefined || email == undefined || password == undefined || type_user == undefined){
+            return res.status(406).json({response_text:"Is not present value"})
+        }
+
+        await conn.connect(function(err) {
+            if (err){
+                conn.end()
+                return res.status(500).json({response_text:"Err of connect"})
+            }
+            
+            const sql = `
+                INSERT INTO user (full_name, date_birth, email, nickname, password, type_user_id, country, city) 
+                VALUES ('${full_name}', '${date_birth}', '${email}', '${nickname}', MD5('${password}'), ${type_user}, '${country}', '${city}')
+            `
+            conn.query(sql, function (err, result, fields) {
+                if (err){
+                    conn.end()
+                    return res.status(500).json({code:err.code,message:err.sqlMessage})
+                }
+                conn.end()
+                if (result.affectedRows === 0){
+                    return res.status(500).json({response_text:"err created user"})
+                }
+                if(result.affectedRows >= 1){
+                    res.status(201).json({response_text:"User Created"})
+                }
+            });
+        });
+    });
+}
