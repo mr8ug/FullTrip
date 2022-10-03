efffa7939c44862aa9226c263e5f47fb5ecd4208
josejfss <josejfss98@gmail.com>
2022-10-02 23:40:54 -0600

    endpoint type_user

--- backend/fulltrip_server/routes/type_user/type_user.js
+++ backend/fulltrip_server/routes/type_user/type_user.js
@@ -4,41 +4,39 @@ var upload = multer({ dest: 'uploads/' })
 
 module.exports = (express,app) => {
     
-	app.post('/api/login',upload.any(), async function(req,res){
+	app.post('/type_user',upload.any(), async function(req,res){
         const conn = await mysql.createConnection(app.config.db.credentials);
-        const {email,password} = req.body
+        //const {email,password} = req.body
         await conn.connect(function(err) {
             if (err){
                 conn.end()
                 return res.status(500).json({response_text:"Err of connect"})
             }
             
-            const sql = `SELECT user_id, full_name ,email,nickname as username,  tu.type_user
-                    FROM user U, type_user tu
-                    WHERE U.email = '${email}' AND U.password = MD5('${password}')
-                    AND tu.type_user_id = U.type_user_id`
+            const sql = `select * from type_user`
             conn.query(sql, function (err, result, fields) {
                 if (err){
                     conn.end()
                     return res.status(500).json({code:err.code,message:err.sqlMessage})
                 }
-                if (result.length === 0){
-                    conn.end()
-                    return res.status(404).json({response_text:"password, email or nickname it's not right"})
-                } 
+                 
                 conn.end()
-               
+               arrTmp = []
                 Object.keys(result).forEach(function(key) {
                     var row = result[key];
-                    
-                    res.status(200).json({ 
-                        userid: row.user_id,
-                        fullname: row.full_name,
-                        username: row.username,
-                        email: row.email,
+                    arrTmp.push({
+                        type_user_id:row.type_user_id,
                         type_user: row.type_user
-                    });
+                    })
+                    // res.status(200).json({ 
+                    //     userid: row.user_id,
+                    //     fullname: row.full_name,
+                    //     username: row.username,
+                    //     email: row.email,
+                    //     type_user: row.type_userd
+                    // });
                 });
+                res.status(200).json({user_type: arrTmp})
             });
         });
     });
