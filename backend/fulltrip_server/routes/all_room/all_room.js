c4158f181b258d6fd76757eaed0e8c78157186bc
Carlos Emilio Campos Moran <carloscamposprivated@gmail.com>
2022-10-05 14:47:03 -0600

    update gethabitaciones => rooms

--- backend/fulltrip_server/routes/all_room/all_room.js
+++ backend/fulltrip_server/routes/all_room/all_room.js
@@ -4,7 +4,7 @@ var upload = multer({ dest: 'uploads/' })
 
 module.exports = (express, app) => {
 
-    app.get('/api/getHabitaciones', upload.any(), async function (req, res) {
+    app.get('/api/all_rooms', upload.any(), async function (req, res) {
         const conn = await mysql.createConnection(app.config.db.credentials)
         const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME
         await conn.connect(function (err) {
